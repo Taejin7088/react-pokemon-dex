742da9143b740863cf0ac12fb5c8031fec7c6c5e
taejin7088 <gog70888@gmail.com>
2025-02-03 17:38:32 +0900

    Design : Pokemoncard 기본적인 디자인 틀 완료

--- src/componets/PokemonCard.jsx
+++ src/componets/PokemonCard.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 
-const PokemonCardStyled = styled.div``;
+const PokemonCardStyled = styled.div`
+  .card {
+    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 15px;
+    margin: 10px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 280px;
+  }
+
+  img {
+    flex-grow: 2;
+  }
+
+  .pokemon-name {
+    flex-grow: 1;
+  }
+
+  .pokemon-num {
+    flex-grow: 1;
+  }
+
+  button {
+    background-color: red;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    cursor: pointer;
+    max-height: 30px;
+    width: 60px;
+    margin-bottom: 10px;
+    flex-grow: 1;
+    transition: 0.3s;
+  }
+  button:hover {
+    background-color: #793434;
+  }
+`;
 
 // {
 //   img_url:
@@ -16,12 +57,14 @@ const PokemonCard = ({ pokemon }) => {
   const paddedId = (pokemon.id + "").padStart(3, "0");
   return (
     <>
-      <div className="card">
-        <img src={pokemon.img_url} alt="포켓몬이미지" />
-        <div className="pokemon-name">{pokemon.korean_name}</div>
-        <div className="pokemon-num">No. {paddedId}</div>
-        <button>추가</button>
-      </div>
+      <PokemonCardStyled>
+        <div className="card">
+          <img src={pokemon.img_url} alt="포켓몬이미지" />
+          <div className="pokemon-name">{pokemon.korean_name}</div>
+          <div className="pokemon-num">No. {paddedId}</div>
+          <button>추가</button>
+        </div>
+      </PokemonCardStyled>
     </>
   );
 };
