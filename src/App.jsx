915f0b793eb6e3ad03bb611cc960e0c6099bf6cd
taejin7088 <gog70888@gmail.com>
2025-02-06 19:59:41 +0900

    Design : alret창 위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <GlobalStyled />
       <ToastContainer
         position="bottom-right" // 알람 위치 지정
-        autoClose={1200} // 자동 off 시간
+        autoClose={1300} // 자동 off 시간
         hideProgressBar={false} // 진행시간바 숨김
         rtl={false} // 알림 좌우 반전
         theme="light"
