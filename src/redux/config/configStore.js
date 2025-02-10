003303b76bd3ccd88c18d2513f7d9c7e73bd592a
taejin7088 <gog70888@gmail.com>
2025-02-10 10:10:34 +0900

    Chore : 미사용코드삭제

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import myPokemonListSlice from "../slices/myPokemonListSlice";
-import { enableMapSet } from "immer";
-enableMapSet();
+
 const store = configureStore({
   reducer: {
     myPokemonList: myPokemonListSlice,
